4e5e23b55d02717421cf3774830d76295118a0a9
蔡三泽 <csz0102@gmail.com>
2022-04-26 17:23:18 +0800

    feat: 更新脚本

--- generateTrimapImages.py
+++ generateTrimapImages.py
@@ -4,17 +4,28 @@ import numpy as np
 import cv2
 
 IMG_EXT = ('.png', '.jpg', '.jpeg', '.JPG', '.JPEG')
-trimap_size = 50
+
+trimap_erode_size = 5
+trimap_dilate_size = 15
 trimap_threshold = 200
+trimap_dilate_value = 128
 
-def trimap(mask, size, conf_threshold):
-    pixels = 2 * size + 1
-    kernel = np.ones((pixels, pixels), np.uint8)
+def trimap(mask, conf_threshold):
+    h, w, _ = mask.shape
+    maxW = max(h, w)
+    scale = maxW / 1000
+    erode_size = int(trimap_erode_size * scale)
 
-    dilation = cv2.dilate(mask, kernel, iterations=2)
+    erode_kernel = np.ones((erode_size, erode_size), np.uint8)
+    mask = cv2.erode(mask, erode_kernel) 
+
+    dilate_Size = 2 * trimap_dilate_size + 1
+    dilate_Size = int(dilate_Size * scale)
+    dilate_kernel = np.ones((dilate_Size, dilate_Size), np.uint8)
+    dilation = cv2.dilate(mask, dilate_kernel, iterations=2)
 
     remake = np.zeros_like(mask)
-    remake[dilation == 255] = 127  # Set every pixel within dilated region as probably foreground.
+    remake[dilation == 255] = trimap_dilate_value  # Set every pixel within dilated region as probably foreground.
     remake[mask > conf_threshold] = 255  # Set every pixel with large enough probability as definitely foreground.
 
     return remake
@@ -31,8 +42,8 @@ def main(cfg):
             continue
         input_image = cv2.imread(os.path.join(input_dir, filename))
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-        trimap_image = trimap(input_image, trimap_size, trimap_threshold)
-        trimap_filename = os.path.basename(filename).split('.')[0] + '.png'
+        trimap_image = trimap(input_image, trimap_threshold)
+        trimap_filename = filename
         cv2.imwrite(os.path.join(trimaps_path, trimap_filename), trimap_image)
         print('done {}'.format(trimap_filename))
 
--- quantization.py
+++ quantization.py
@@ -1,8 +1,20 @@
 import coremltools as ct
 from coremltools.models.neural_network import quantization_utils
+import argparse
 
-# load full precision model
-model_original = ct.models.MLModel('inpaint.mlmodel')
+def main(cfg):
+    input_path = cfg.input_path
+    output_path = cfg.output_path
 
-model_fp8 = quantization_utils.quantize_weights(model_original, nbits=8, quantization_mode='linear')
-model_fp8.save('inpaint_8_1.mlmodel')
+    # load full precision model
+    model_original = ct.models.MLModel(input_path)
+
+    model_fp8 = quantization_utils.quantize_weights(model_original, nbits=16, quantization_mode='linear')
+    model_fp8.save(output_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path', type=str, default=None)
+    parser.add_argument('--output_path', type=str, default=None)
+    config = parser.parse_args()
+    main(config)
