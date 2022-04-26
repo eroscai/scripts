774bf66c478937954252030a192d3150451b55f8
蔡三泽 <csz0102@gmail.com>
2022-04-26 22:54:34 +0800

    feat: 更新trimap生成脚本

--- generateTrimapImages.py
+++ generateTrimapImages.py
@@ -1,34 +1,36 @@
 import argparse
 import os
+from matplotlib import image
 import numpy as np
 import cv2
+from scipy.ndimage.morphology import binary_erosion
 
 IMG_EXT = ('.png', '.jpg', '.jpeg', '.JPG', '.JPEG')
 
-trimap_erode_size = 5
-trimap_dilate_size = 15
-trimap_threshold = 200
-trimap_dilate_value = 128
+trimap_erode_size = 10
+trimap_fill_value = 128
 
-def trimap(mask, conf_threshold):
-    h, w, _ = mask.shape
-    maxW = max(h, w)
-    scale = maxW / 1000
-    erode_size = int(trimap_erode_size * scale)
 
-    erode_kernel = np.ones((erode_size, erode_size), np.uint8)
-    mask = cv2.erode(mask, erode_kernel) 
+def trimap(
+    mask,
+    foreground_threshold=250,
+    background_threshold=5,
+):
+    is_foreground = mask > foreground_threshold
+    is_background = mask < background_threshold
+    structure = None
+    if trimap_erode_size > 0:
+        structure = np.ones(
+            (trimap_erode_size, trimap_erode_size), dtype=np.int32)
 
-    dilate_Size = 2 * trimap_dilate_size + 1
-    dilate_Size = int(dilate_Size * scale)
-    dilate_kernel = np.ones((dilate_Size, dilate_Size), np.uint8)
-    dilation = cv2.dilate(mask, dilate_kernel, iterations=2)
+    is_foreground = binary_erosion(is_foreground, structure=structure)
+    is_background = binary_erosion(is_background, structure=structure, border_value=1)
 
-    remake = np.zeros_like(mask)
-    remake[dilation == 255] = trimap_dilate_value  # Set every pixel within dilated region as probably foreground.
-    remake[mask > conf_threshold] = 255  # Set every pixel with large enough probability as definitely foreground.
+    trimap = np.full(mask.shape, dtype=np.uint8, fill_value=trimap_fill_value)
+    trimap[is_foreground] = 255
+    trimap[is_background] = 0
 
-    return remake
+    return trimap
 
 
 def main(cfg):
@@ -40,12 +42,14 @@ def main(cfg):
     for filename in images_list:
         if not filename.endswith(IMG_EXT):
             continue
+
         input_image = cv2.imread(os.path.join(input_dir, filename))
-        input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-        trimap_image = trimap(input_image, trimap_threshold)
+        input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+        trimap_image = trimap(input_image)
         trimap_filename = filename
         cv2.imwrite(os.path.join(trimaps_path, trimap_filename), trimap_image)
         print('done {}'.format(trimap_filename))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
