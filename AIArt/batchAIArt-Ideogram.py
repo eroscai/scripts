967e086341c604cab94486bdc88c9a14a5f45720
蔡三泽 <csz0102@gmail.com>
2024-09-10 00:00:03 +0800

    feat: 更新脚本

--- AIArt/batchAIArt-Ideogram.py
+++ AIArt/batchAIArt-Ideogram.py
@@ -37,7 +37,8 @@ def read_json_files(directory, download_directory, cover_ids):
     json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
     json_files.sort()  # 按照字母数字排序
 
-    data = []
+    data_v1 = []
+    data_v2 = []
     file_index = 1
 
     for file in json_files:
@@ -53,27 +54,43 @@ def read_json_files(directory, download_directory, cover_ids):
                 width, height = trim_size(result.get('width', 0), result.get('height', 0))
 
                 cover_id = result.get('cover_response_id', '')
+                model_version = result.get('model_version', '')
+                is_v2 = model_version == 'V_1_5'
                 cover_url = 'https://ideogram.ai/assets/progressive-image/balanced/response/' + cover_id
                 filename = f'{file_index}.jpg'
-                target_url = download_directory + filename
+                if is_v2:
+                    target_url = download_directory + '/v2/' + filename
+                else:
+                    target_url = download_directory + '/v1/' + filename
+
                 success = download_image(cover_url, target_url)
                 # success = True
                 if success:
-                    if cover_id not in cover_ids:                        
-                        data.append({
+                    if cover_id not in cover_ids:
+                        data = {
                             "prompt": prompt,
+                            'model_version': model_version,
                             "aspect_ratio": result.get('aspect_ratio', ''),
                             "cover_response_id": cover_id,
                             'width': width,
                             'height': height,
                             'filename': filename
-                        })
+                        }
+                        if is_v2 :
+                            data_v2.append(data)
+                        else:
+                            data_v1.append(data)
                     file_index += 1
 
-    return data
+    return (data_v1, data_v2)
 
 # 下载图片函数
 def download_image(url, save_path):
+    # 已存在
+    if os.path.exists(save_path):
+        print(f"已存在： {save_path}")
+        return True
+
     # 获取保存路径的目录部分
     directory = os.path.dirname(save_path)
 
@@ -85,7 +102,7 @@ def download_image(url, save_path):
     print(f"开始下载: {url}")
     # 设置请求头，模拟浏览器
     headers = {
-        'Cookie': '__cf_bm=gVN7AKLpmXE4Gyug.deBk82CSC5vwtvoE7yy64dJ.tc-1725809197-1.0.1.1-CgG4b8UFbdaj_KHD6ZMUTm_T0lCknrTTD7ymE9BxIjJ98cDT8sW_IKRoL7Sj9BoEWIfZLAFv2DjYhDNmFzB5uQ',
+        'Cookie': '__cf_bm=rXHmkIBqMYrQ4Jx6o25lyMwmf4BiFoByen9WMotaKtw-1725897411-1.0.1.1-U8lIZ8Vl5Tfk4vTO4XxX5WZGSJ2oVaqJGo81dlYPeK0ifYr8xjPmHUFJlA2BJ9BFJOu90d7McfnTiEjLoCMr5A; _ga_44J8R31CP6=GS1.1.1725894895.1.1.1725894906.0.0.0; AMP_da0464495c=JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI2M2I3YmQ4Zi1kY2RjLTQ0YWQtYjExYS1kMWFkNjNmNjUwMzUlMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzI1ODk0ODk1NjI1JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTcyNTg5NDkwMTQwNiUyQyUyMmxhc3RFdmVudElkJTIyJTNBOCUyQyUyMnBhZ2VDb3VudGVyJTIyJTNBMiU3RA==; _ga=GA1.1.1478484158.1725894896; cf_clearance=xL3nwk3cL.tAVkmvcVGQeV7z5hhpka9xLJIgfPEv3UU-1725894896-1.2.1.1-Z3oVctYcUwm.KtkeZaqGOPepqH_Lg1_.JY9BKM2Tl7nnIPKobs_wmygHTxJtviwXG4ZFaEDVxbkpsLW7pNv8oVsisNS2Twb3D4kTaSnNoifZFzPu3.w8ISPn_9MIY1vlmlTtwZ68Z.Ci8SW8j8LbIOpAjuDZJJVe_wMu_5t3IXRXmpZL5.LDArIPCUBlWaDgOLuSxLjKoqX5X.MZys_mFzT_7ivefEIUnU4rJ6j5TIVXN.DCUNiftDb7mLWfe9QILLcj2LkqMUi88KHTGAKnfn9obfXVXFj3Yyd5xHa7dNWZRLcznTHprAEeH6jxBPwh6ShvypppS_5FN2XxQBmcFWsdfVo5D7LLzeu2djOcxBQugD30sujyR4HZS0L52bPmSj7jdk6jErM4GK9AY1HeLA; AMP_MKTG_da0464495c=JTdCJTdE',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15'
     }
     # 发送GET请求
@@ -160,21 +177,32 @@ def save_to_excel(data, output_file):
     df = pd.DataFrame(data)
     df.to_excel(output_file, index=False, engine='openpyxl')
 
-def main(directory, output_file, download_directory):
-    response_file = '/Users/Eros/Downloads/Ideogram/3d/output.xlsx'
-    cover_ids = extract_fields(response_file, 'Sheet1', 'cover_response_id')
+def main(directory, download_directory, cover_ids):
+    (data_v1, data_v2) = read_json_files(directory, download_directory, cover_ids)
 
-    data = read_json_files(directory, download_directory, cover_ids)
-    save_to_excel(data, output_file)
-    print(f'Data has been successfully saved to {output_file}')
+    output_file1 = directory + '/output_v1.xlsx'
+    save_to_excel(data_v1, output_file1)
+    output_file2 = directory + '/output_v2.xlsx'
+    save_to_excel(data_v2, output_file2)
+
+    print(f'Data has been successfully saved to {output_file1} {output_file2}')
 
 if __name__ == '__main__':
-    directory = '/Users/Eros/Downloads/Ideogram/3d/'  # 替换为你的JSON文件所在目录
-    download_directory = directory + '/Download/'
-    output_file = directory + '/output.xlsx'
-    # main(directory, output_file, download_directory)
+    main_directory = '/Users/Eros/Downloads/Ideogram-week/'
+    directory = main_directory + 'realistic/'  # 替换为你的JSON文件所在目录
+    download_directory = directory + '/Download'
+
+    output_files = main_directory + 'outputs'
+    excel_files = [f for f in os.listdir(output_files) if f.endswith('.xlsx')]
+    cover_ids = []
+    for excel_file in excel_files:
+        excel_file_path = output_files + '/' + excel_file
+        ids = extract_fields(excel_file_path, 'Sheet1', 'cover_response_id')
+        cover_ids.append(ids)
+
+    main(directory, download_directory, cover_ids)
 
     # clean_directory(directory)
 
-    json_file = directory + '/output.json'
-    excel_to_json(output_file, "Sheet1", json_file)
+    # json_file = directory + '/output.json'
+    # excel_to_json(output_file, "Sheet1", json_file)
